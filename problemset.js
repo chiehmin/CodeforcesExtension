0dceeebe67c5efb07a0e2d664b363ec53a096d58
FatMinMin <chiehmin18@gmail.com>
2016-08-29 23:15:05 +0800

    adding url starts with www into matches

--- problemset.js
+++ problemset.js
@@ -1,7 +1,7 @@
 var curUrl = $(location).attr("href");
-var arr = /http:\/\/codeforces.com\/problemset\/problem\/(\d+)\/(\w)/.exec(curUrl);
-var probSet = arr[1];
-var probNo = arr[2];
+var arr = /http:\/\/(www.)?codeforces.com\/problemset\/problem\/(\d+)\/(\w)/.exec(curUrl);
+var probSet = arr[2];
+var probNo = arr[3];
 var statusUrl = "http://codeforces.com/problemset/status/" + probSet +  "/problem/" + probNo;
 var statusLinkElement = $("<a>").attr("href", statusUrl)
                                 .text("Submission Status");
